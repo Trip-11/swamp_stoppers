c1e5948b293151346cb8feca40429f7149c335ac
Trip-11 <28yountd@bwschools.net>
2025-05-07 11:46:39 -0400

    commit

--- main.py
+++ main.py
@@ -12,19 +12,27 @@ class Game:
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         self.running = True
+        self.font_name = pg.font.match_font(FONT_NAME)
 
     def new(self):
-        # start a new game
-        self.all_sprites = pg.sprite.Group()
+        self.score=0
+        self.all_sprites = pg.sprite.LayeredUpdates()
         self.platforms = pg.sprite.Group()
+        self.powerups = pg.sprite.Group()
+        self.clouds = pg.sprite.Group()
         self.player = Player(self)
         self.all_sprites.add(self.player)
         for plat in PLATFORM_LIST:
-            p = Platform(*plat)
-            self.all_sprites.add(p)
-            self.platforms.add(p)
+            Platform(self,*plat)
+        self.cloud_images = []
+        for i in range(1, 4):
+            filename = "cloud{}.png".format(i)
+            img = pg.image.load(filename)
+            self.cloud_images.append(img)
+        for i  in range (8):
+                c=Cloud(self)
+                c.rect.y+=500
         self.run()
-
     def run(self):
         # Game Loop
         self.playing = True
@@ -45,17 +53,22 @@ class Game:
                 self.player.vel.y = 0
         #if player reaches 1/4 of screen
         if self.player.rect.top<= HEIGHT/4:
+            if random.randrange(100)<15:
+                Cloud(self)
             self.player.pos.y += abs(self.player.vel.y)
+            for cloud in self.clouds:
+                    cloud.rect.y += abs(self.player.vel.y)
             for plat in self.platforms:
                 plat.rect.y += abs(self.player.vel.y)
                 if plat.rect.top >= HEIGHT:
                     plat.kill()
+                    self.score += 10
         #spawn new platforms average of 5
         while len(self.platforms) < 6:
             width=random.randrange(50,101)
             x = random.randrange(0,WIDTH-width)
             y = random.randrange(-75, -30)
-            p = Platform(x,y,width,20)
+            p = Platform(self,x,y,width,20)
             self.platforms.add(p)
             self.all_sprites.add(p)
         #die
@@ -66,7 +79,11 @@ class Game:
                     sprite.kill()
         if len(self.platforms) == 0:
             self.playing = False
-
+        pow_hits = pg.sprite.spritecollide(self.player,self.powerups, True)
+        for pow in pow_hits:
+            if pow.type == 'boost':
+                self.player.vel.y = -BOOST_POWER
+                self.player.jumping=False
 
 
 
@@ -81,23 +98,61 @@ class Game:
                 self.running = False
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE:
+
+                    self.jumping = True
                     self.player.jump()
+
+
 
     def draw(self):
         # Game Loop - draw
-        self.screen.fill(BLACK)
+        self.screen.fill(GREEN)
         self.all_sprites.draw(self.screen)
+        self.draw_text(self.screen, str(self.score),22, WIDTH/2,15,WHITE)
         # *after* drawing everything, flip the display
         pg.display.flip()
 
     def show_start_screen(self):
         # game splash/start screen
-        pass
+        # game splash/start screen
+        self.screen.fill(BLUE)
+        time = pg.time.get_ticks()
+        # self.draw_text(self.screen, 'Platform Game', 64, WIDTH / 2, HEIGHT / 4, WHITE)
+        self.draw_text(self.screen, 'Arrow Keys to Move', 22, WIDTH / 2, HEIGHT / 2, WHITE)
+        self.draw_text(self.screen, 'Press A to Begin', 18, WIDTH / 2, HEIGHT * 3 / 4, RED)
+        for i in range(WIDTH//2+100):
+            self.screen.fill(BLUE)
+            self.draw_text(self.screen, 'Swamp Stompers', 64, -100+i, HEIGHT / 4, WHITE)
+            pg.display.flip()
+        pg.display.flip()
+        waiting = True
+        while waiting:
 
+            color = random.choice([RED, WHITE])
+            self.draw_text(self.screen, 'Press A to Begin', 18, WIDTH / 2, HEIGHT * 3 / 4, color)
+
+            pg.display.flip()
+            self.clock.tick(FPS)
+            for event in pg.event.get():
+                keystate = pg.key.get_pressed()
+                if event.type == pg.QUIT:
+                    pg.quit()
+                if event.type == pg.KEYDOWN:
+                    if keystate[pg.K_a]:
+                        waiting = False
     def show_go_screen(self):
         # game over/continue
         pass
 
+
+    def draw_text(self, surf, text, size, x, y, Color):
+        self.font_name = pg.font.match_font('arial')
+        font = pg.font.Font(self.font_name, size)
+        text_surface = font.render(text, True, Color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        surf.blit(text_surface, text_rect)
+        
 g = Game()
 g.show_start_screen()
 while g.running:
@@ -105,3 +160,13 @@ while g.running:
     g.show_go_screen()
 
 pg.quit()
+
+
+
+#trees on either side
+#less branches
+#jump power if held longer
+#background
+#diff powers
+#score
+#spawn on branch on side
--- sprites.py
+++ sprites.py
@@ -1,60 +1,85 @@
 # Sprite classes for platform game
 from operator import truediv
-
 import pygame as pg
 from settings import *
 vec = pg.math.Vector2
+import random
+
 
 class Player(pg.sprite.Sprite):
     def __init__(self, game):
+        self._layer = PLAYER_LAYER
         pg.sprite.Sprite.__init__(self)
         self.game = game
+        self.width = 60
+        self.height = 30
         self.idle_img = []
+        self.run_img_r = []
+        self.run_img_l = []
+        self.jump_img_r = []
+        self.jump_img_l=[]
         self.load_images()
         self.image = self.idle_img[0]
-        self.run_img_r=[]
-        self.run_img_l = []
+
         self.running = False
         # self.image = pg.Surface((30, 40))
         # self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
-        self.pos = vec(WIDTH / 2, HEIGHT / 2)
+        self.rect.center = (100, HEIGHT / 2)
+        self.pos = vec(100, HEIGHT / 2)
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
         self.last_update = 0
         self.current_frame = 0
+        self.jumping=False
 
     def animate(self):
         now=pg.time.get_ticks()
-        if self.vel.x != 0:
+        if int(self.vel.x) != 0:
             self.running = True
         else:
             self.running = False
-        if not self.running:
+            self.jumping = False
+        if not self.running and not self.jumping:
             if now-self.last_update>350:
                 self.last_update=now
                 self.current_frame = (self.current_frame+1)%len(self.idle_img)
                 self.image = self.idle_img[self.current_frame]
                 self.rect = self.image.get_rect()
-            if self.running:
-                if now - self.last_update > 350:
-                    self.last_update = now
-                    self.current_frame = (self.current_frame + 1) % len(self.idle_img)
-                    self.image = self.idle_img[self.current_frame]
+        if self.running and not self.jumping:
+
+            if now - self.last_update > 150:
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.run_img_r)
+                if self.vel.x>0:
+                    self.vel.y -= 6
+                    self.image = self.run_img_r[self.current_frame]
+                else:
+                    self.vel.y -= 6
+                    self.image = self.run_img_l[self.current_frame]
+
+
+
     def load_images(self):
         for i in range(1,4):
-            filename = "idle{}.png".format(i)
+            filename = "frog{}.png".format(i)
             img=pg.image.load(filename)
             #img = pg.transform.scale(img,(self.width,self.height))
             self.idle_img.append(img)
-        for i in range(1, 7):
-            filename = "run{}.png".format(i)
+        for i in range(1, 5):
+            filename = "frogjump{}.png".format(i)
             img = pg.image.load(filename)
             # img = pg.transform.scale(img,(self.width,self.height))
             self.run_img_r.append(img)
-        for frame in self.run_img_l:
+        for frame in self.run_img_r:
             self.run_img_l.append(pg.transform.flip(frame,True,False))
+        for i in range(1, 5):
+            filename = "frogjump{}.png".format(i)
+            img = pg.image.load(filename)
+            # img = pg.transform.scale(img,(self.width,self.height))
+            self.jump_img_r.append(img)
+        for frame in self.run_img_r:
+            self.jump_img_l.append(pg.transform.flip(frame,True,False))
     def jump(self):
         # jump only if standing on a platform
         self.rect.x += 1
@@ -62,7 +87,17 @@ class Player(pg.sprite.Sprite):
         self.rect.x -= 1
         if hits:
             self.vel.y = -20
-
+            now = pg.time.get_ticks()
+            if now - self.last_update > 50:
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.run_img_r)
+                if self.vel.x > 0:
+                    self.vel.y -= 6
+                    self.image = self.jump_img_r[self.current_frame]
+                else:
+                    self.vel.y -= 6
+                    self.image = self.jump_img_l[self.current_frame]
+        # self.jumpimg = False
     def update(self):
         self.animate()
         self.acc = vec(0, PLAYER_GRAV)
@@ -86,18 +121,100 @@ class Player(pg.sprite.Sprite):
         self.rect.midbottom = self.pos
 
 class Platform(pg.sprite.Sprite):
-    def __init__(self, x, y, w, h):
-        pg.sprite.Sprite.__init__(self)
+    def __init__(self, game,x, y, w, h):
+        self._layer = PLATFORM_LAYER
+        self.groups = game.all_sprites,game.platforms
+        self.game=game
+        self.groups = game.all_sprites, game.platforms
+        pg.sprite.Sprite.__init__(self, self.groups)
+        # pg.sprite.Sprite.__init__(self)
+        # self.image = pg.Surface((w, h))
+        # self.image.fill(GREEN)
+        # self.rect = self.image.get_rect()
+
+        self.plat_img = []
+        self.side = random.randint(1, 2)
+        #print(self.side)
+        if self.side == 1:
+            x = random.randrange(0, WIDTH // 4)
+
+        else:
+            x = random.randrange(3 * WIDTH // 4, WIDTH)
+            print('2')
+        self.load_images()
+        print(self.side)
+
+        self.image = random.choice(self.plat_img)
+        self.rect = self.image.get_rect()
+
+
+        self.rect.centerx = x
+        self.rect.y = y
+        # self.width = 25
+        # self.height = 10
+        if random.randrange(100)<POW_SPAWN_PCT:
+            Pow(self.game,self)
+    def load_images(self):
+
+        if self.side == 1:
+            for i in range(1, 4):
+                filename = "platform{}.png".format(i)
+                img = pg.image.load(filename)
+                if i ==2 or i == 3:
+                    img = pg.transform.flip(img, True, False)
+
+
+                img = pg.transform.scale(img,(150,50))
+                self.plat_img.append(img)
+        else:
+            for i in range(1, 4):
+                filename = "platform{}.png".format(i)
+                img = pg.image.load(filename)
+                if i == 1 or i == 4:
+                    img = pg.transform.flip(img, True, False)
+
+                img = pg.transform.scale(img, (150, 50))
+                self.plat_img.append(img)
+
+
+
+
+class Cloud(pg.sprite.Sprite):
+    def __init__(self, game):
+        self._layer = CLOUD_LAYER
+        self.groups = game.all_sprites, game.clouds
+        pg.sprite.Sprite.__init__(self,self.groups)
+        self.game=game
+        self.image = random.choice(self.game.cloud_images)
+        self.image.set_colorkey(BLACK)
         # self.image = pg.Surface((w, h))
         # self.image.fill(GREEN)
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.plat_img = []
-        self.load_image()
-        self.image = random.choice(self.plat_img)
-    def loag_images(self):
-        for i in range(1,5):
-            filename = "platform{}.PNG".format(i)
-            img=pg.image.load(filename)
-            self.plat_img.append(img)
+        scale = random.randrange(50,101) / 100
+        self.image= pg.transform.scale(self.image,(int(self.rect.width*scale),int(self.rect.height*scale)))
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-500,-50)
+    def update(self):
+      if self.rect.top> HEIGHT*2:
+        self.kill()
+
+
+class Pow(pg.sprite.Sprite):
+    def __init__(self, game, plat):
+        self.groups = game.all_sprites, game.powerups
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.plat = plat
+        self.type = random.choice(['boost'])
+        self.image = pg.image.load("power.PNG")
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = self.plat.rect.centerx
+        self.rect.bottom = self.plat.rect.top - 5
+
+    def update(self):
+        self.rect.bottom = self.plat.rect.top - 5
+        if not self.game.platforms.has(self.plat):
+            self.kill()
+
+
